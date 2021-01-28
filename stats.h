8b333faf2e5e239e4f54865ef44957dee448a649
santukulkarni091 <77432629+santukulkarni091@users.noreply.github.com>
2021-01-28 13:22:04 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -1,5 +1,11 @@
 #include <vector>
 
 namespace Statistics {
-    Stats ComputeStatistics(const std::vector<___>& );
+    class stats
+    {
+        float x,y,z,max,min,avg; 
+        stats();
+        stats(float X, float Y, float Z); 
+        void ComputeStatistics();
+    }
 }
