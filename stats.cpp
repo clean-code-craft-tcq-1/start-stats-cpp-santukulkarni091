d81cfa0cb849d54247d92908954e522f11210825
santukulkarni091 <77432629+santukulkarni091@users.noreply.github.com>
2021-01-28 14:46:58 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,21 @@
 #include "stats.h"
+using namespace std;
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
+namespace Statistics
+{
+   stats::stats(float X, float Y, float Z)
+   {
+       x = X;
+       y = Y;
+       z = Z;
+   }
+void stats::ComputeStatistics() 
+{
     //Implement statistics here
+    avg = (x + y + z)/3;
+    max = std::max({x, y, z});
+    min = std::min({x, y, z});
+    
+}
+
 }
